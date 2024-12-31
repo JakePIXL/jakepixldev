ce0f315567c194783414bf3dbe92c6af80eab6f7
Jake Evans <jakewjevans@pm.me>
2024-12-31 03:44:21 -0500

    added content to rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("posts");
@@ -11,6 +14,9 @@ export async function GET(context) {
     items: posts.map((post) => ({
       title: post.data.title,
       description: post.data.description,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
       pubDate: post.data.publishDate,
       link: `posts/${post.slug}`,
     })),
